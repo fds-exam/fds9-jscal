fe099d9f642f3b38bcaadaa7a5d3e47ff6394208
chiabi <chiabi88@gmail.com>
2018-04-28 19:52:43 +0900

    윈도우 연산 순서대로 만들었으나 아이폰 계산기와 연산 순서가 달라서 수정예정

--- calculator.js
+++ calculator.js
@@ -1,127 +1,172 @@
-// 아름다운 코드
-
-// 화면에 보이는 숫자는 문자열로.
-// 처음 숫자를 누르면 저장, 숫자 뒤 숫자를 누르면 문자열로 뒤에 추가.
-
-// ac
-// neg
-// percent
-// divide
-// multi
-// minus
-// plus
-// eql
-
-const calc = {
-  display: '',
-  accCount: 0,
-  beforeCount: 0,
-  state: '',
-  // common() {
-  //   this.beforeCount = Number(this.display);
-  //   this.accCount += Number(this.display);
-  //   this.display = '';
-  // },
-  plus() {
-    this.beforeCount = Number(this.display);
-    this.accCount += Number(this.display);
-    this.display = '';
-    if(!this.display) {
-      display.textContent = this.accCount + '';
-    } else {
-      this.accCount = (this.accCount + Number(this.display)) + '';
-    }
-    display.textContent = this.accCount;
-  },
-  minus() {
-    this.beforeCount = Number(this.display);
-    this.accCount -= Number(this.display);
-    this.display = '';
-    if(!this.display) {
-      display.textContent = this.accCount + '';
-    } else {
-      this.accCount = (this.accCount - Number(this.display)) + '';
-    }
-    display.textContent = this.accCount;
-  },
-  // multi() {
-  //   this.beforeCount = Number(this.display);
-  //   this.accCount += Number(this.display);  // 3
-  //   this.display = ''; // 0
-  //   if(!this.display) {
-  //     display.textContent = this.accCount + '';
-  //   } else {
-  //     this.accCount = (this.accCount * Number(this.display)) + '';
-  //   }
-  //   display.textContent = this.accCount;
-  // },
-  // divide() {
-  //   this.beforeCount = Number(this.display);
-  //   this.accCount += Number(this.display);  // 3
-  //   this.display = ''; // 0
-  //   if(!this.display) {
-  //     display.textContent = this.accCount + '';
-  //   } else {
-  //     this.accCount = (this.accCount + Number(this.display)) + '';
-  //   }
-  //   display.textContent = this.accCount;
-  // }
-}
-
 const display = document.querySelector('.display');
+function write(str) {
+  display.textContent = str;
+}
+const calc = {
+  input: '', // 입력 문자열
+  acc: '', // 누적 문자열
+  save: '' // 계속되는 계산을 위한 저장 표현식
+};
 
-document.querySelectorAll('button').forEach(el => {
-  el.addEventListener('click', (e) => {
-    // 숫자를 문자열로 display
-    if(e.target.classList[0].slice(0, 4) === 'num-') {
-      if (e.target.classList[0].slice(4) === 'dot') {
-        calc.display += !calc.display.includes('.') ? (!calc.display ? '0.' : '.') : '';
-        display.textContent = calc.display;
-      } else if (e.target.classList[0].slice(4) === '0' && calc.display === '') {
-        display.textContent = '0';
-      } else {
-        calc.display += e.target.classList[0].slice(4);
-        display.textContent = calc.display;
-      }
-    } 
-  });
+// ac 모든 계산값이 초기화
+document.querySelector('.op-ac').addEventListener('click', () => {
+  calc.input = '';
+  calc.acc = 0;
+  calc.save = 0;
+  write(calc.acc);
 });
 
-document.querySelectorAll('.op').forEach(el => {
-  el.addEventListener('click', (e)=> {
-    switch(e.target) {
-      case document.querySelector('.op-ac'):
-        calc.beforeCount = 0;
-        calc.state = '';
-        calc.display = '';
-        calc.accCount = 0;
-        display.textContent = calc.accCount;
-        break;
-      case document.querySelector('.op-plus'):
-        calc.plus();
-        calc.state = 'plus';
-        break;
-      case document.querySelector('.op-minus'):
-        calc.minus();
-        calc.state = 'minus';
-        break;
-      case document.querySelector('.op-eql'): 
-        switch(calc.state) {
-          case 'plus':
-            calc.beforeCount = Number(calc.display);
-            calc.accCount += calc.beforeCount;
-            display.textContent = calc.accCount;
-            break;
-          case 'minus':
-            calc.beforeCount = Number(calc.display);
-            calc.accCount -= calc.beforeCount;
-            display.textContent = calc.accCount;
-            break;
-          default:
-            display.textContent = calc.accCount;
-            break;
-        }
-        break; 
-    }
-  });
+// neg 
+document.querySelector('.op-neg').addEventListener('click', () => {
+  if(calc.input === '') {
+    calc.acc = calc.acc * -1;
+    write(calc.acc);
+  } else {
+    calc.input = calc.input * -1;
+    write(calc.input);
+  }
+});
+
+// percent
+// document.querySelector('.op-percent').addEventListener('click', () => {
+//   if(calc.input === '') {
+//     calc.input = calc.acc * (1 / 100);
+//     write(calc.input);
+//     console.log('입력이 없으면', calc.input);
+//   } else {
+//     calc.acc = eval(calc.input * (1 / 100))
+//     write(calc.acc);
+//     console.log('입력이 있으면', calc.input);
+//     calc.input = '';
+//   }
+// });
+
+// plus
+document.querySelector('.op-plus').addEventListener('click', () => {
+  // 누적값이 없으면
+  if (!calc.acc) {
+    calc.save = calc.input + '+';  
+    calc.acc = eval(calc.save.slice(0,  -1));
+    console.log(`누적값이 없으면${calc.save}`);
+    write(calc.acc);
+    // 누적값이 있으면.
+  } else if(calc.input === '' && !/\d/.test(calc.save.slice(-1))){
+    calc.save = calc.save.slice(0,  -1) + '+'; 
+    write(calc.acc);
+    console.log(`입력이 없으면: ${calc.acc}`);
+    // 누적값은 있고 입력은 없고, 'save +'이런식으로 되어 있지 않을때 
+  } else {
+    console.log(`입력이 있으면: ${calc.save}${calc.input}`);
+    calc.acc =  eval(`${calc.save}${calc.input}`);
+    calc.save = calc.acc + '+'; 
+    console.log(calc.save);
+    write(calc.acc);
+  }
+  calc.input = '';
+});
+
+// minus
+document.querySelector('.op-minus').addEventListener('click', () => {
+  if (calc.acc === '') {
+    calc.save = calc.input + '-';  
+    calc.acc = eval(calc.save.slice(0,  -1));
+    console.log(`누적값이 없으면${calc.save}`);
+    write(calc.acc);
+    // 누적값이 있으면.
+  } else if(calc.input === '' && !/\d/.test(calc.save.slice(-1))){
+    calc.save = calc.save.slice(0,  -1) + '-'; 
+    write(calc.acc);
+    console.log(`입력이 없으면: ${calc.acc}`);
+  }  else {
+    console.log(`입력이 있으면: ${calc.save}${calc.input}`);
+    calc.acc =  eval(`${calc.save}${calc.input}`);
+    calc.save = calc.acc + '-'; 
+    console.log(calc.save);
+    write(calc.acc);
+  }
+  calc.input = '';
+});
+
+// multi
+document.querySelector('.op-multi').addEventListener('click', () => {
+  if (calc.acc === '') {
+    calc.save = calc.input + '*';    
+    calc.acc = eval(calc.save.slice(0,  -1));
+    console.log(`누적값이 없으면${calc.save}`);
+    write(calc.acc);
+    // 누적값이 있으면.
+  } else if(calc.input === '' && !/\d/.test(calc.save.slice(-1))){
+    calc.save = calc.save.slice(0,  -1) + '*'; 
+    write(calc.acc);
+    console.log(`입력이 없으면: ${calc.acc}`);
+  }  else {
+    console.log(`입력이 있으면: ${calc.save}${calc.input}`);
+    calc.acc =  eval(`${calc.save}${calc.input}`);
+    calc.save = calc.acc + '*'; 
+    console.log(calc.save);
+    write(calc.acc);
+  }
+  calc.input = '';
+});
+
+// divide
+document.querySelector('.op-divide').addEventListener('click', () => {
+  if (calc.acc === '') {
+    calc.save = calc.input + '/';    
+    calc.acc = eval(calc.save.slice(0,  -1));
+    console.log(`누적값이 없으면${calc.save}`);
+    write(calc.acc);
+    // 누적값이 있으면.
+  } else if(calc.input === '' && !/\d/.test(calc.save.slice(-1))){
+    calc.save = calc.save.slice(0,  -1) + '/'; 
+    write(calc.acc);
+    console.log(`입력이 없으면: ${calc.acc}`);
+  }  else {
+    console.log(`입력이 있으면: ${calc.save}${calc.input}`);
+    calc.acc =  eval(`${calc.save}${calc.input}`);
+    calc.save = calc.acc + '/'; 
+    console.log(calc.save);
+    write(calc.acc);
+  }
+  calc.input = '';
+});
+document.querySelector('.op-eql').addEventListener('click', () => {
+
+
+});
+
+
+
+// 임시 숫자 키패드
+document.querySelector('.num-1').addEventListener('click', () => {
+  calc.input += '1';
+  write(calc.input);
+});
+document.querySelector('.num-2').addEventListener('click', () => {
+  calc.input += '2';
+  write(calc.input);
+});
+document.querySelector('.num-3').addEventListener('click', () => {
+  calc.input += '3';
+  write(calc.input);
+});
+document.querySelector('.num-4').addEventListener('click', () => {
+  calc.input += '4';
+  write(calc.input);
+});
+document.querySelector('.num-5').addEventListener('click', () => {
+  calc.input += '5';
+  write(calc.input);
+});
+document.querySelector('.num-6').addEventListener('click', () => {
+  calc.input += '6';
+  write(calc.input);
+});
+document.querySelector('.num-7').addEventListener('click', () => {
+  calc.input += '7';
+  write(calc.input);
+});
+document.querySelector('.num-0').addEventListener('click', () => {
+  calc.input += '0';
+  write(calc.input);
 });
