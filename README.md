ea8454cc4a7bec844905b539e25d16c8cc36fe5a
chiabi <chiabi88@gmail.com>
2018-05-02 18:21:42 +0900

    연산부분 함수로 옮겨서 조금 정리. 지수 표기법이랑 나중에 세자리에 콤마 추가할 거 방법 메모

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 | 일자 | 수정 내용 |
 | --- | ---- |
 | 180428 | 윈도우 계산기처럼 `2 + 2 * 3` 이런 연산에서 순서대로 `2 + 2`, `4 * 3`으로 계산되어 결과값이 `12`가 나오게 만들었는데 아이폰에서는 `2 + 2 * 3`의 경우 중간에 출력되는 값(`4`) 없이 연산 우선순위대로 `2 + (2 * 3)`으로 연산을 수행해 `8`이 결과값으로 나오는 걸 확인해서 다시 수정해야 함...(목표는 아이폰 계산기처럼 동작이었으므로...) |
+
+## 테스트
+
+아이폰 계산기의 최대 표기 가능 수는 억단위 임   
+소수점 입력 가능한 자리수도 `0.00000001`까지
+
+`%`버튼을 계속 누르면 `0.00000001`이하로는 지수 표기법으로 변환 됨
++ [`numObj.toExponential()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toExponential)
+```js
+(0.0000000001).toExponential();
+// "1e-10"
+```
+
+화면에 결과 출력할 때 세자리 수에 `,` 출력하는 부분 
+```js
+function write(str) {
+	const acc = '' + eval(str);
+	return [...acc].reduce((acc, item, index, arr) => index % 3 === (arr.length % 3) && index !== 0 ? acc + ',' + item : acc + item, '');
+}
+write('3 * (10 ** 9)')
+// "3,000,000,000"
+
+write('3 * (10 ** 8)')
+// "300,000,000"
+```
--- calculator.js
+++ calculator.js
@@ -1,35 +1,123 @@
 const display = document.querySelector('.display');
+// 화면에 입력하는 함수
 function write(str) {
   display.textContent = str;
 }
+// 계산 관련
 const calc = {
   input: '', // 입력 문자열
   acc: '', // 누적 문자열
-  save: '', // 계속되는 계산을 위한 저장 표현식
+  save: '', // 계속되는 계산을 위한 저장 문자열
   temp_input: '',
   temp_save: '',
   temp_percent: '',
 };
+/*
+ * calc.save는 아이폰 계산기에서 연속되는 입력에는 연산자 속성이 바뀔경우 중간 출력되는 경우를 위함
+ * calc.save의 저장은 곱셈, 나눗셈 연산에서만 이뤄짐
+ */
 
-// ac 모든 계산값이 초기화
-document.querySelector('.op-ac').addEventListener('click', () => {
-  calc.input = '';
-  calc.acc = '';
+// 덧셈과 뺄샘 연산
+function calcPlusMinus(op) {
+  // 2 + 2 +   3 * 2
+  calc.acc = calc.acc + calc.save;
+  // 연산자가 재입력되어 바뀔 경우를 위해 calc.save는 임시로 저장하고 비운다.
+  calc.temp_save = calc.save; 
   calc.save = '';
-  calc.temp_input = '';
-  calc.temp_save = '';
-  write('0');
-});
-
-// neg 
-document.querySelector('.op-neg').addEventListener('click', () => {
-  if(calc.input === '') {
-    calc.input = '-';
-    write(calc.input + '0');
+  // 만약 입력이 있으면
+  if (calc.input !== '') {
+    // 입력에 연산자를 더한 문자열은 누적 문자열로 한다.
+    calc.acc += calc.input + op;
+    // 입력된 숫자는 뒤의 연산자가 바뀔경우를 위해 임시로 저장하고 비운다.
+    calc.temp_input = calc.input;
+    calc.input = '';
   } else {
-    calc.input = calc.input * -1;
-    write(calc.input);
-  }
+    // 만약 없으면 마지막에 더해진 연산자를 제거하고 새로운 연산자를 추가 
+    calc.acc = calc.acc.slice(0, -1) + op;
+  };
+  // 연산자를 빼고 누적 결과의 연산된 결과를 출력한다.
+  write(eval(calc.acc.slice(0, -1)));
+  console.log(`덧셈 뺄셈의 누적 결과: ${calc.acc}`);
+}
+
+// 곱셈과 나눗셈 연산
+function calcMultiDivide(op) {
+  if (calc.input === '' && calc.save) {
+    // 입력은 없고 calc.save가 남아있는 경우 (이전에 입력한 연산이 *나 /)
+    // 저장된 calc.save가 우선하므로 임시 save는 지운다.
+    // 연산자를 교체한다.
+    calc.temp_save = '';
+    calc.save = calc.save.slice(0, -1) + op;
+  } else if (calc.input === '' && !calc.save) {
+    // 입력은 없고, 저장된 calc.save도 없는경우(이전에 연산이 +나 -)
+    if (calc.temp_save !== '') {
+      // 임시로 저장된 temp_save가 있다는 것은 
+      // 예를들어 그 전 문자열이 `2 * 3 +`인경우 `2 *`이 문자열이 저장되어 있음 
+      // 누적 문자열에서 그 전에 계산처리된 부분 제거하고 저장 문자열을 연산자를 바꿔 교체
+      calc.acc = calc.acc.substr(0, calc.acc.length - calc.temp_save.length - calc.temp_input.length - 1);
+      calc.save = calc.temp_save + calc.temp_input + op;
+    } else {
+      // 임시로 저장된 temp_save가 없고 임시 저장한 temp_input은 있는경우
+      // 예를들어 그 전 문자열이 `33 +`인 경우 `33`이 저장되어 있음
+      // 누적 문자열에서 그 전에 계산처리된 부분 제거하고 저장 문자열에 새로 담는다.
+      calc.acc = calc.acc.substr(0, calc.acc.length - calc.temp_input.length - 1);
+      calc.save = calc.temp_input + op;  
+    }
+  } else {
+    // 입력이 있는 경우
+    // 임시 저장된 temp_save는 지우고 save에 입력과 연산을 계속 저장한다.
+    calc.temp_save = '';
+    calc.save += calc.input + op; 
+    calc.temp_input = calc.input;
+    calc.input = '';
+  };
+  // 연산자를 지우고 임시 저장된 save 값의 연산 결과를 보여줌
+  write(eval(calc.save.slice(0, -1)));
+  console.log(`곱셈과 나눗셈의 누적 결과: ${calc.acc} 와 저장 문자열: ${calc.save}`);
+}
+
+document.querySelectorAll('[class*="op"]').forEach(item => {
+  item.addEventListener('click', e => {
+    const itemClass = item.classList;
+    if(itemClass.contains('op-plus')) {
+      calcPlusMinus('+');
+    } else if (itemClass.contains('op-minus')) {
+      calcPlusMinus('-');
+    } else if (itemClass.contains('op-multi')) {
+      calcMultiDivide('*');
+    } else if (itemClass.contains('op-divide')) {
+      calcMultiDivide('%');
+    } else if (itemClass.contains('op-neg')) {
+      if(calc.input === '') {
+        calc.input = '-';
+        write(calc.input + '0');
+      } else {
+        calc.input = calc.input * -1;
+        write(calc.input);
+      }
+    } else if (itemClass.contains('op-ac')) {
+      calc.input = '';
+      calc.acc = '';
+      calc.save = '';
+      calc.temp_input = '';
+      calc.temp_save = '';
+      write('0');
+    } else if (itemClass.contains('op-eql')) {
+      if (calc.input === '' && calc.save !== '') {
+        // if(calc.temp_save === '') {
+        //   calc.acc = calc.save + calc.save;
+        //   calc.temp_save = calc.save;
+        //   calc.save = '';
+        // } else {
+        //   calc.acc += calc.temp_save;
+        // }
+        write(eval(calc.acc.slice(0, -1)));
+      } else if (calc.input === '' && calc.save === '') {
+        calc.acc += calc.temp_input + calc.acc.slice(-1);
+      }
+      console.log(`= 결과: ${calc.acc} 와 저장 문자열: ${calc.save}`);
+    }
+  });
 });
 
 // percent
@@ -46,115 +134,6 @@ document.querySelector('.op-neg').addEventListener('click', () => {
 //     calc.input = '';
 //   }
 // });
-
-// plus
-document.querySelector('.op-plus').addEventListener('click', () => {
-  // 입력이 없을때
-  // 입력, calc.acc 뒤에 연산자 있을때
-  calc.acc = calc.acc + calc.save;
-  calc.temp_save = calc.save; 
-  console.log(`temp save: ${calc.temp_save}`);
-  calc.save = '';
-  if (calc.input !== '') {
-    calc.acc += calc.input + '+';
-  } else {
-    // if(calc.temp_input === '') {
-      calc.acc = calc.acc.slice(0, -1) + '+';
-    // } else {
-    //   console.log(`temp_input 동작 이상할 수 있음`);
-    //   calc.acc += calc.temp_input + '+';
-    // }
-  };
-  console.log(`minus plus : ${calc.acc}`);
-  write(eval(calc.acc.slice(0, -1)));
-  calc.temp_input = calc.input;
-  calc.input = '';
-
-});
-
-// minus
-document.querySelector('.op-minus').addEventListener('click', () => {
-  calc.acc = calc.acc + calc.save;
-  calc.temp_save = calc.save; 
-  console.log(`temp save: ${calc.temp_save}`);
-  calc.save = '';
-  if (calc.input === '') {
-    calc.acc = calc.acc.slice(0, -1) + '-'
-  } else {
-    calc.acc += calc.input + '-';
-  };
-  console.log(`minus acc : ${calc.acc}`);
-  write(eval(calc.acc.slice(0, -1)));
-  calc.temp_input = calc.input;
-  calc.input = '';
-
-});
-
-// multi
-document.querySelector('.op-multi').addEventListener('click', () => {
-  if (calc.input === '' && calc.save) {
-    console.log('boo');
-    calc.temp_save = '';
-    calc.save = calc.save.slice(0, -1) + '*';
-    console.log(`calc.save: ${calc.save}, calc.acc: ${calc.acc}`);
-  } else if (calc.input === '' && !calc.save) {
-    console.log('yoo');
-    if (calc.temp_save !== '') {
-      console.log(`temp save: ${calc.temp_save}`);
-      console.log(calc.acc.substr(0, calc.acc.length - calc.temp_save.length - calc.temp_input.length - 1));
-      calc.acc = calc.acc.substr(0, calc.acc.length - calc.temp_save.length - calc.temp_input.length - 1);
-      calc.save = calc.temp_save + calc.temp_input + '*';
-    } else {
-      console.log(calc.acc.substr(0, calc.acc.length - calc.temp_input.length - 1));
-      calc.acc = calc.acc.substr(0, calc.acc.length - calc.temp_input.length - 1);
-      calc.save = calc.temp_input + '*';  
-    }
-    // calc.save = calc.acc.slice(0, -1) + '*';
-    console.log(`calc.save: ${calc.save}, calc.acc: ${calc.acc}`);
-  } else {
-    console.log('foo');
-    calc.temp_save = '';
-    calc.save += calc.input + '*'; 
-    console.log(`calc.save: ${calc.save}, calc.acc: ${calc.acc}`);
-  };
-  // calc.acc = calc.acc + calc.save;
-  write(eval(calc.save.slice(0, -1)));
-  calc.temp_input = calc.input;
-  calc.input = '';
-});
-
-// divide
-document.querySelector('.op-divide').addEventListener('click', () => {
-  if (calc.input === '' && calc.save) {
-    console.log('boo');
-    calc.temp_save = '';
-    calc.save = calc.save.slice(0, -1) + '/';
-    console.log(`calc.save: ${calc.save}, calc.acc: ${calc.acc}`);
-  } else if (calc.input === '' && !calc.save) {
-    console.log('yoo');
-    if (calc.temp_save !== '') {
-      console.log(`temp save: ${calc.temp_save}`);
-      console.log(calc.acc.substr(0, calc.acc.length - calc.temp_save.length - calc.temp_input.length - 1));
-      calc.acc = calc.acc.substr(0, calc.acc.length - calc.temp_save.length - calc.temp_input.length - 1);
-      calc.save = calc.temp_save + calc.temp_input + '/';
-    } else {
-      console.log(calc.acc.substr(0, calc.acc.length - calc.temp_input.length - 1));
-      calc.acc = calc.acc.substr(0, calc.acc.length - calc.temp_input.length - 1);
-      calc.save = calc.temp_input + '/';  
-    }
-    // calc.save = calc.acc.slice(0, -1) + '/';
-    console.log(`calc.save: ${calc.save}, calc.acc: ${calc.acc}`);
-  } else {
-    console.log('foo');
-    calc.temp_save = '';
-    calc.save += calc.input + '/'; 
-    console.log(`calc.save: ${calc.save}, calc.acc: ${calc.acc}`);
-  };
-  // calc.acc = calc.acc + calc.save;
-  write(eval(calc.save.slice(0, -1)));
-  calc.temp_input = calc.input;
-  calc.input = '';
-});
 document.querySelector('.op-percent').addEventListener('click', () => {
   if (calc.input === '' && calc.temp_save) {
     console.log('poo');
@@ -180,43 +159,17 @@ document.querySelector('.op-percent').addEventListener('click', () => {
     calc.input = '';
   };
 });
-document.querySelector('.op-eql').addEventListener('click', () => {
 
-
-});
-
-
-
-// 임시 숫자 키패드
-document.querySelector('.num-1').addEventListener('click', () => {
-  calc.input += '1';
-  write(calc.input);
-});
-document.querySelector('.num-2').addEventListener('click', () => {
-  calc.input += '2';
-  write(calc.input);
-});
-document.querySelector('.num-3').addEventListener('click', () => {
-  calc.input += '3';
-  write(calc.input);
-});
-document.querySelector('.num-4').addEventListener('click', () => {
-  calc.input += '4';
-  write(calc.input);
-});
-document.querySelector('.num-5').addEventListener('click', () => {
-  calc.input += '5';
-  write(calc.input);
-});
-document.querySelector('.num-6').addEventListener('click', () => {
-  calc.input += '6';
-  write(calc.input);
-});
-document.querySelector('.num-7').addEventListener('click', () => {
-  calc.input += '7';
-  write(calc.input);
-});
-document.querySelector('.num-0').addEventListener('click', () => {
-  calc.input += '0';
-  write(calc.input);
+// 숫자 키패드
+document.querySelectorAll('[class^="num-"]').forEach(el => {
+  el.addEventListener('click', e => {
+    const num = el.classList[0].slice(4);
+    if ( num === 'dot') {
+      calc.input += calc.input === '' ? '0.' : '.';
+      write(calc.input);
+    } else {
+      calc.input += num;
+      write(calc.input);
+    }
+  });
 });
